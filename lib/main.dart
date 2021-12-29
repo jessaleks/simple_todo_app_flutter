18ca3090f03d28fb18287e1cb8ffe9df1692a6d5
Aleksander Jess <jess.aleksander@gmail.com>
2021-12-29 22:17:50 +0100

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app_flutter/todo_list.dart';
+// ignore: unnecessary_import
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+import 'package:todo_app_flutter/views/todo_list.dart';
+import 'package:todo_app_flutter/views/add_todo_screen.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('testBox');
   runApp(const MyApp());
 }
 
@@ -12,20 +19,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const TodoList(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: "/",
+        routes: {
+          '/': (context) => const TodoList(),
+          "/add_todo": (context) => const AddTodoScreen(),
+        });
   }
 }
