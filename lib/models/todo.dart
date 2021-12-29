caa1f5d5926c9a59f278f619becbf7d9e08233d5
Aleksander Jess <jess.aleksander@gmail.com>
2021-12-29 22:18:02 +0100

    set up models

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,11 +1,15 @@
+import 'package:hive/hive.dart';
 import 'package:uuid/uuid.dart';
+
+part 'todo.g.dart';
 
 String generateUUID() {
   const uuid = Uuid();
   return uuid.v4();
 }
 
-class Todo {
+@HiveType(typeId: 0)
+class Todo extends HiveObject {
   Todo({
     required this.name,
     required this.isComplete,
@@ -13,15 +17,21 @@ class Todo {
     this.description,
   });
 
+  @HiveField(0)
   final String id = generateUUID();
 
+  @HiveField(1)
   String name;
 
+  @HiveField(2)
   bool isComplete = false;
 
+  @HiveField(3)
   String? description;
 
+  @HiveField(4)
   final DateTime createdAt = DateTime.now().toUtc();
 
+  @HiveField(5)
   DateTime updatedAt = DateTime.now().toUtc();
 }
---
+++ lib/models/todo.g.dart
@@ -0,0 +1,54 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TodoAdapter extends TypeAdapter<Todo> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Todo read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Todo(
+      name: fields[1] as String,
+      isComplete: fields[2] as bool,
+      updatedAt: fields[5] as DateTime,
+      description: fields[3] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Todo obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.isComplete)
+      ..writeByte(3)
+      ..write(obj.description)
+      ..writeByte(4)
+      ..write(obj.createdAt)
+      ..writeByte(5)
+      ..write(obj.updatedAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TodoAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
